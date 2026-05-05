edb6fa84b96a49a67251c378e951770db586cb2f
Jefferson <jeffersonlv@gmail.com>
2026-05-05 14:29:58 +0100

    add users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,9 +16,9 @@ class UserSeeder extends Seeder
             $company = \App\Models\Company::first();
         }
 
-        User::create(['name' => 'Super Admin', 'username' => 'super',  'email' => 'super@itech.com',  'password' => Hash::make('password'), 'role' => 'superadmin', 'company_id' => null]);
-        User::create(['name' => 'Admin',       'username' => 'admin',  'email' => 'admin@sabor.com',  'password' => Hash::make('password'), 'role' => 'admin',      'company_id' => $company->id]);
-        User::create(['name' => 'Gerente Ana', 'username' => 'ana',    'email' => 'ana@sabor.com',    'password' => Hash::make('password'), 'role' => 'manager',    'company_id' => $company->id]);
-        User::create(['name' => 'Staff Bruno', 'username' => 'bruno',  'email' => 'bruno@sabor.com',  'password' => Hash::make('password'), 'role' => 'staff',      'company_id' => $company->id]);
+        User::create(['name' => 'Super Admin', 'username' => 'super',  'email' => 'jeffersonlv@gmail.com',  'password' => Hash::make('0F3b8123!@#'), 'role' => 'superadmin', 'company_id' => null]);
+        User::create(['name' => 'Jonatas',     'username' => 'jonatas',  'email' => 'jonatas@craicandcoffee.com',  'password' => Hash::make('testeteste'), 'role' => 'admin',      'company_id' => $company->id]);
+        User::create(['name' => 'Manager Craic', 'username' => 'managercraic',    'email' => 'manager@craicandcoffee.com',    'password' => Hash::make('testeteste'), 'role' => 'manager',    'company_id' => $company->id]);
+        User::create(['name' => 'Staff Craic', 'username' => 'staffcraic',  'email' => 'staff@craicandcoffee.com',  'password' => Hash::make('testeteste'), 'role' => 'staff',      'company_id' => $company->id]);
     }
 }
